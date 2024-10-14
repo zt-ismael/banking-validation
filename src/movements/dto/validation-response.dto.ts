cc31d7684cfc92861b501941fa7c56cd038a5af4
zt-ismael <zakari.toure.ismael@gmail.com>
2024-10-14 13:57:03 +0200

    Update : Imporving and Typing validation API response (including API documentation)

--- src/movements/dto/validation-response.dto.ts
+++ src/movements/dto/validation-response.dto.ts
@@ -1,17 +1,49 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { MovementDto } from './validate-movements.dto';
 
+enum ValidationErrorType {
+    BalanceMismatch = 'balance_mismatch',
+    DuplicateTransaction = 'duplicate_transaction',
+}
+
+
+export class ValidationError {
+    @ApiProperty({ description: 'The type of validation error', enum: ValidationErrorType })
+    type: ValidationErrorType;
+  
+    @ApiProperty({ description: 'The date of the balance mismatch or duplicate transaction', type: Date })
+    date: Date;
+  
+    @ApiProperty({ description: 'Suggested fixes for the balance mismatch', type: String })
+    suggested_fixes: string;
+  
+    @ApiPropertyOptional({ description: `The expected balance (for "${ValidationErrorType.BalanceMismatch}" error type ONLY)`, type: Number })
+    expected_balance?: number;
+  
+    @ApiPropertyOptional({ description: `The calculated balance (for "${ValidationErrorType.BalanceMismatch}" error type ONLY)`, type: Number })
+    calculated_balance?: number;
+  
+    @ApiPropertyOptional({ description: `The missing amount (for "${ValidationErrorType.BalanceMismatch}" error type ONLY)`, type: Number })
+    missing_amount?: number;
+  
+    @ApiPropertyOptional({ description: `The duplicate operation (for "${ValidationErrorType.DuplicateTransaction}" error type ONLY)`, type: MovementDto })
+    duplicate_operation?: MovementDto;
+  
+    @ApiPropertyOptional({ description: `Number of duplicate operation occurences (for "${ValidationErrorType.DuplicateTransaction}" error type ONLY)`, type: Number })
+    occurences?: number;
+  }
 
 export class ValidationResponseDto {
-    @ApiProperty({ description: 'Indicates whether the validation was successful or not' })
+    @ApiProperty({ description: 'Indicates whether the validation was successful or not', type: Boolean })
     success: boolean;
 
-    @ApiProperty({ description: 'A message describing the result of the validation' })
+    @ApiProperty({ description: 'A message describing the result of the validation', type: String })
     message: string;
 
-    @ApiProperty({ description: 'An optional array of reasons for the validation failure' })
-    reasons?: string[];
+    @ApiProperty({ description: 'An optional array of reasons for the validation failure', type: [ValidationError] })
+    reasons?: ValidationError[];
 
-    constructor(success: boolean, message: string, reasons?: string[]) {
+    constructor(success: boolean, message: string, reasons?: ValidationError[]) {
         this.success = success;
         this.message = message;
         this.reasons = reasons;
